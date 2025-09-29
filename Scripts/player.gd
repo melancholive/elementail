562853cb57430d5b95b0fcc5a80ec9f6ff3776c4
V-run64 <vpandian4u@gmail.com>
2025-09-28 22:40:57 -0400

    Player Character
    Added animation and actions for player character

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,9 +1,12 @@
 extends CharacterBody2D
 
 @export var speed: float = 100.0
+@onready var anim: AnimatedSprite2D = $AnimatedSprite2D
+var last_direction: Vector2 = Vector2.ZERO
 
 func _process(delta):
 	var direction = Vector2.ZERO
+
 
 	# WASD or Arrow keys
 	if Input.is_action_pressed("ui_right") or Input.is_key_pressed(KEY_D):
@@ -18,6 +21,41 @@ func _process(delta):
 	# normalize so diagonal speed isn't faster
 	if direction != Vector2.ZERO:
 		direction = direction.normalized()
+		last_direction = direction
 
 	velocity = direction * speed
 	move_and_slide()
+	
+	
+	if Input.is_key_pressed(KEY_SPACE):
+		_play_shoot_animation(last_direction)
+	elif direction != Vector2.ZERO:
+		_play_walk_animation(direction)
+	else:
+		_play_idle_animation(last_direction)
+		
+func _play_shoot_animation(direction: Vector2):
+	if abs(direction.x) > abs(direction.y):
+		anim.play("shootright" if direction.x > 0 else "shootleft")
+	else:
+		anim.play("shootdown" if direction.y > 0 else "shootup")
+		
+func _play_walk_animation(direction: Vector2):
+	if abs(direction.x) > abs(direction.y):
+		if direction.x > 0:
+			anim.play("walkright")
+		else:
+			anim.play("walkleft")
+	else:
+		if direction.y > 0:
+			anim.play("walkdown")
+		else:
+			anim.play("walkup")
+	
+	
+	
+func _play_idle_animation(direction: Vector2):
+	if abs(direction.x) > abs(direction.y):
+		anim.play("idleright" if direction.x > 0 else "idle_left")
+	else:
+		anim.play("idledown" if direction.y > 0 else "idle_up")
